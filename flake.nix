ef1f210abd3d1b8e72a0303171c3479473183275
tofixrs <tofix.rs@proton.me>
2024-07-23 10:01:06 +0200

    feat: add cachix to the flake

--- flake.nix
+++ flake.nix
@@ -18,4 +18,8 @@
         ./parts
       ];
     });
+  nixConfig = {
+    substituters = ["https://sm-gui-editor.cachix.org"];
+    trusted-public-keys = ["sm-gui-editor.cachix.org-1:sOHlz7KeslsRSBiGfE5G4Hbm7URIdtaI1zZSeL92m3s="];
+  };
 }
