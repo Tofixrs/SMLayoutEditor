6b118d88833feee3a3976606786f5db03c98b4e4
tofixrs <tofix.rs@proton.me>
2024-08-05 15:44:06 +0200

    fix: more metadata fixes
    maybe builds in ci, doesnt build locally

--- parts/shell.nix
+++ parts/shell.nix
@@ -4,13 +4,21 @@ _: {
     self',
     ...
   }: {
-    devShells.default = pkgs.mkShell {
-      name = "shell";
-      inputsFrom = [self'.packages.configGen];
-    };
-    devShells.mygui = pkgs.mkShell {
-      name = "mygui-shell";
-      inputsFrom = [self'.packages.mygui];
+    devShells = {
+      default = pkgs.mkShell {
+        name = "shell";
+        inputsFrom = [self'.packages.configGen];
+      };
+      mygui = pkgs.mkShell {
+        name = "mygui-shell";
+        inputsFrom = [self'.packages.mygui];
+      };
+      flatpak = pkgs.mkShell {
+        name = "flatpak";
+        packages = with pkgs; [
+          appstream
+        ];
+      };
     };
   };
 }
